851d373e10b485af380b936f2adb57fdf7ef8632
CancuiW <can.cui@wayne.edu>
2023-09-12 10:31:54 -0700

    finished

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,11 +1,32 @@
+const jwt=require('jsonwebtoken')
+const { JWT_SECRET } = require('./../../config/index')
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
+  const token=req.headers.authorization
+  if(token){
+    jwt.verify(token, JWT_SECRET,(err,decoded)=>{
+      if(err){
+        next({status:401,message:`token bad:${err.message}`})
+      }else{
+        req.decodedJwt=decoded
+        //{ subject: 2, username: 'can', iat: 1694497858, exp: 1694584258 }
+        console.log(req.decodedJwt)
+        next()
+      }
+    })
+  }else{
+    next({status:401, message:"no tokens"})
+  }
 }
 
 // AUTHORIZATION
-const checkRole = (req, res, next) => {
-  next()
+const checkRole = role=>(req, res, next) => {
+  if (req.decodedJwt && req.decodedJwt.username===role){
+    next()
+  }else{
+    next({status:403,message:'you have no power here!'})
+  }
+    
 }
 
 module.exports = {
